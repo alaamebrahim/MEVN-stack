1628a9e0386d27de14e8be5d6ee019ccb7a54ab3
alaa mohammed <alaamebrahim91@gmail.com>
2019-05-31 19:39:20 +0200

    remove unnecessary styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -38,30 +38,4 @@ body {
     }
   }
 }
-
-.vue-notification {
-  padding: 10px;
-  margin: 5px;
-
-  font-size: 12px;
-
-  color: #ffffff;
-  background: #44a4fc;
-  border-left: 5px solid #187fe7;
-
-  &.warn {
-    background: #ffb648;
-    border-left-color: #f48a06;
-  }
-
-  &.error {
-    background: green;
-    border-left-color: #b82e24;
-  }
-
-  &.success {
-    background: #68cd86;
-    border-left-color: #42a85f;
-  }
-}
 </style>
