3c8988c15d516ce49c764d57026462a2e62cfb57
alaa mohammed <alaamebrahim91@gmail.com>
2019-06-06 01:19:07 +0200

    randomize jwt token secret

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,5 +1,5 @@
 const authInfo = {
-  jwtPassKey: "ame@2019ju$T$4mE",
+  jwtPassKey: "ame@2019TgJubGhERtgKHG$%H&Bk",
   issuer: "alaamebrahim91@gmail.com",
   audience: "http://localhost:3000",
   "userIdKey": 'userId@users',
